06a1be8e10a5e40d0b93f68d25182fc8fdfdc980
aishwarya-s10 <78895527+aishwarya-s10@users.noreply.github.com>
2023-11-07 23:45:13 +0100

    Test Cases for all fucntions

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import Generate_Random_Integer, Get_Random_Operator, Generate_Mathematical_Expression_Result
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,22 +9,27 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = Generate_Random_Integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        # Test if the random operator is one of the expected operators
+        operators = ['+', '-', '*']
+        for _ in range(1000):  # Test a large number of random operators
+            rand_operator = Get_Random_Operator()
+            self.assertTrue(rand_operator in operators)
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (10, 4, '-', '10 - 4', 6),
+                (3, 6, '*', '3 * 6', 18)
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                problem, answer = generate_mathematical_expression_result(num1, num2, operator)
+                self.assertEqual(problem, expected_problem)
+                self.assertEqual(answer, expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
